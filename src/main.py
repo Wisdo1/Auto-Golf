b8df5966808c9ace92d1d6b3e0ee3ea856356ed9
Wh1zz299 <wisdomafolabi22@gmail.com>
2025-04-28 15:14:01 -0400

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       wisdo                                                        #
+# 	Created:      4/28/2025, 3:13:11 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
